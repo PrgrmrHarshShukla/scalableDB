10290048cedcf7590e7dede3ab8426d1728b0687
HARSH SHUKLA <ashuklaharsh2005@gmail.com>
2023-11-19 11:03:57 +0530

    Filter display modified

--- frontend/src/components/SearchBar.tsx
+++ frontend/src/components/SearchBar.tsx
@@ -1,17 +1,68 @@
 import '@fortawesome/fontawesome-free/css/all.css'
+import { useState } from 'react'
 
-function SearchBar() {
+interface FilterDataType {
+    [key: string]: string
+}
+
+const SearchBar: React.FC = () => {
+    const [searchData, setSearchData] = useState<FilterDataType[]>([]);
+    const [searchDisplay, setSearchDisplay] = useState<string[]>([]);
+    const [filter, setFilter] = useState<string>("");
+    const [filterValue, setFilterValue] = useState<string>("");
+
+    const addInputValue = () => {
+        if(!filter || !filterValue || filter === "Select filter"){
+            alert("Please select a filter.")
+        }
+        
+        const constraint = {
+            filter: filterValue
+        }
+
+        setSearchData((prev) => {
+            if(prev){
+                return [
+                    ...prev,
+                    constraint
+                ]
+            }
+            else{
+                return [
+                    constraint
+                ]
+            }
+        })
+        setSearchDisplay((prev) => {
+            if(prev){
+                return [
+                    ...prev,
+                    `  ${filter}: ${filterValue}`
+                ]
+            }
+            else{
+                return [`  ${filter}: ${filterValue}`]
+            }
+        });
+
+        setFilter("");
+        setFilterValue("");
+    }
+
+
+
+
     return(
         <div className="w-screen h-auto flex flex-col justify-center items-center gap-4">
             <div className="w-screen h-[10vh] flex flex-row justify-center items-center">
-                <input placeholder="Search" type="text" className="text-black px-2 text-black w-[80vw] sm:w-[60vw] h-[5vh] border-[2px] border-white rounded-tl-[10px] rounded-bl-[10px] bg-white" />
+                <input value={searchDisplay} readOnly placeholder="Search" type="text" className="font-semibold text-black px-2 text-black w-[80vw] sm:w-[60vw] h-[5vh] border-[2px] border-white rounded-tl-[10px] rounded-bl-[10px] bg-white" />
                 <div className="cursor-pointer w-12 h-[5vh] bg-blue-600 hover:bg-blue-700 active:bg-blue-900 flex flex-row justify-center items-center border-[2px] border-white rounded-tr-[10px] rounded-br-[10px]">
                     <i className="fas fa-search text-white"></i>
                 </div>            
             </div>
 
             <div className="w-screen h-auto flex flex-row justify-center items-center">
-                <select className="pl-2 pb-[2px] h-[4vh] border-[2px] border-white rounded-bl-[10px] rounded-tl-[10px] bg-white">
+                <select className="pl-2 pb-[2px] h-[4vh] border-[2px] border-white rounded-bl-[10px] rounded-tl-[10px] bg-white" onChange={(e) => setFilter(e.target.value)} value={filter}>
                     <option>Select filter</option>
                     <option>level</option>
                     <option>message</option>
@@ -21,8 +72,8 @@ function SearchBar() {
                     <option>commit</option>
                     <option>metadata.parentResourceId</option>
                 </select>
-                <input type="text" placeholder="Filter value" className="pb-[2px] text-black h-[4vh] border-l-[2px] border-gray-800 rounded-br-[10px] rounded-tr-[10px] bg-white pl-2" />
-                <div className="flex flex-row justify-center items-center cursor-pointer ml-20 border-[0.5px] border-white rounded-[10px] px-4 h-[4vh] bg-blue-600 hover:bg-blue-700 active:bg-blue-900 font-semibold">
+                <input value={filterValue} onChange={(e) => setFilterValue(e.target.value)} type="text" placeholder="Filter value" className="pb-[2px] text-black h-[4vh] border-l-[2px] border-gray-800 rounded-br-[10px] rounded-tr-[10px] bg-white pl-2" />
+                <div className="flex flex-row justify-center items-center cursor-pointer ml-20 border-[0.5px] border-white rounded-[10px] px-4 h-[4vh] bg-blue-600 hover:bg-blue-700 active:bg-blue-900 font-semibold" onClick={addInputValue}>
                     Add Filter
                 </div>
             </div>
